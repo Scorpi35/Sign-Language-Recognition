75cd729d8d4104555419ded087f3ba9f68424293
ashish <subediashish35@gmail.com>
2020-10-31 11:56:46 +0545

    Fit the CNN model

--- train.py
+++ train.py
@@ -48,6 +48,30 @@ train_datagen = ImageDataGenerator(
 
 test_datagen = ImageDataGenerator(rescale=1./255)
 
+training_set = train_datagen.flow_from_directory(
+    "data/training_set",
+    target_size=(64, 64),
+    batch_size=32,
+    class_mode='categorical'
+)
+
+test_set = test_datagen.flow_from_directory(
+    "data/test_set",
+    target_size=(64, 64),
+    batch_size=32,
+    class_mode='categorical'
+)
+
+model = classifier.fit_generator(
+    training_set,
+    steps_per_epoch=800,
+    epochs=25,
+    validation_data=test_set,
+    validation_steps=6500
+)
+
+
+
 
 
 
