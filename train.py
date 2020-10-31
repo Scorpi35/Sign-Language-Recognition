c96d780b82fec9bedada98831dd0628a9974f7ab
ashish <subediashish35@gmail.com>
2020-10-31 11:42:19 +0545

    Add second, third convulation layers and faltten layer

--- train.py
+++ train.py
@@ -14,3 +14,16 @@ classifier.add(Convolution2D(32, 3, 3, input_shape=(64, 64, 3), activation="relu
 
 # Pooling
 classifier.add(MaxPooling2D(pool_size=(2, 2)))
+
+# Second convolution layer
+classifier.add(Convolution2D(32, 3, 3, activation="relu"))
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
+
+# Third convolution layer
+classifier.add(Convolution2D(64, 3, 3, activation="relu"))
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
+
+# Flattening
+classifier.add(Flatten())
+
+
