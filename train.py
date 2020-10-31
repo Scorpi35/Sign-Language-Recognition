e2e93fc3381f449d1351afe7de6046045e9b9a5c
ashish <subediashish35@gmail.com>
2020-10-31 11:52:31 +0545

    Add test datagen

--- train.py
+++ train.py
@@ -39,6 +39,15 @@ classifier.compile(
     metrics=['accuracy']
 )
 
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True
+)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
 
 
 
