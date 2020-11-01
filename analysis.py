dedb39482cc362134206be06c692c3cedc29d33d
ashish <subediashish35@gmail.com>
2020-11-01 12:17:52 +0545

    Generate train_datagen

--- analysis.py
+++ analysis.py
@@ -4,6 +4,7 @@ from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense, Dropout
 from keras import optimizers
+from keras.preprocessing.image import ImageDataGenerator
 
 classifier = Sequential()
 
@@ -17,5 +18,25 @@ classifier.add(Convolution2D(32, (3,  3), activation='relu'))
 classifier.add(MaxPooling2D(pool_size=(2, 2)))
 
 classifier.add(Flatten())
+
+classifier.add(Dense(256, activation='relu'))
+classifier.add(Dropout(0.5))
+classifier.add(Dense(26, activation='softmax'))
+
+# Generating image data
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True
+)
+
+training_set = train_datagen.flow_from_directory(
+        'data/training_set',
+        target_size=(64, 64),
+        batch_size=32,
+        class_mode='categorical')
+
+
 
 
